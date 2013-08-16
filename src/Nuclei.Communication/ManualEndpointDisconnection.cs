25239f8cde58adb6f47548a1ad9d9c7442d0daf2
Petrik van der Velde <petrikvandervelde@gmail.com>
2013-08-16 23:55:07 +1200

    Adding a delegate for the manual disconnection of an endpoint.

---
+++ src/Nuclei.Communication/ManualEndpointDisconnection.cs
@@ -0,0 +1,19 @@
+﻿//-----------------------------------------------------------------------
+// <copyright company="Nuclei">
+//     Copyright 2013 Nuclei. Licensed under the Apache License, Version 2.0.
+// </copyright>
+//-----------------------------------------------------------------------
+
+namespace Nuclei.Communication
+{
+    /// <summary>
+    /// A delegate used to manually disconnect from an endpoint.
+    /// </summary>
+    /// <remarks>
+    /// This delegate is mainly to be used through the <see cref="CommunicationModule"/> where this 
+    /// delegate is registered.
+    /// </remarks>
+    /// <param name="endpointId">The endpoint ID of the remote endpoint.</param>
+    /// <param name="channelType">The channel type of the remote endpoint.</param>
+    public delegate void ManualEndpointDisconnection(EndpointId endpointId, ChannelType channelType);
+}
