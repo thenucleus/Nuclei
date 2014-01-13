d419a520766bbb680b20a2df6530f5b72274bd4a
Petrik van der Velde <petrikvandervelde@gmail.com>
2014-01-13 22:34:47 +1300

    Adding the versions of the discovery protocol.
    references #3

---
+++ src/nuclei.communication/Discovery/DiscoveryVersions.cs
@@ -0,0 +1,38 @@
+﻿//-----------------------------------------------------------------------
+// <copyright company="P. van der Velde">
+//     Copyright (c) P. van der Velde. All rights reserved.
+// </copyright>
+//-----------------------------------------------------------------------
+
+using System;
+
+namespace Nuclei.Communication.Discovery
+{
+    /// <summary>
+    /// Defines the known versions of the discovery layer.
+    /// </summary>
+    internal static class DiscoveryVersions
+    {
+        /// <summary>
+        /// Gets the current version of the discovery layer.
+        /// </summary>
+        public static Version Current
+        {
+            get
+            {
+                return Base;
+            }
+        }
+
+        /// <summary>
+        /// Gets the version of the base discovery layer.
+        /// </summary>
+        public static Version Base
+        {
+            get
+            {
+                return new Version(1, 0, 0, 0);
+            }
+        }
+    }
+}
