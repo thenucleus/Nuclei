8263aedd70e955eb3430ad8a709b53fd1ba63e8a
Petrik van der Velde <petrikvandervelde@gmail.com>
2014-01-15 09:17:14 +1300

    Adding XML comments to the IDiscoveryEndpoint interface
    references #3

--- src/nuclei.communication/Discovery/IDiscoveryEndpoint.cs
+++ src/nuclei.communication/Discovery/IDiscoveryEndpoint.cs
@@ -1,5 +1,14 @@
-﻿namespace Nuclei.Communication.Discovery
+﻿//-----------------------------------------------------------------------
+// <copyright company="Nuclei">
+//     Copyright 2013 Nuclei. Licensed under the Apache License, Version 2.0.
+// </copyright>
+//-----------------------------------------------------------------------
+
+namespace Nuclei.Communication.Discovery
 {
+    /// <summary>
+    /// Defines the base interface for WCF endpoints in the discovery layer.
+    /// </summary>
     internal interface IDiscoveryEndpoint : IReceiveInformationFromRemoteEndpoints
     {
     }
