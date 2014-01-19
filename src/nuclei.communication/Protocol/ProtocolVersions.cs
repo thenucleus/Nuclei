f46dc7cb358545ecf2a5f8085e8503559aaf4c33
Petrik van der Velde <petrikvandervelde@gmail.com>
2014-01-20 08:55:55 +1300

    Adding the protocol versions
    references #3

---
+++ src/nuclei.communication/Protocol/ProtocolVersions.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Nuclei.Communication.Protocol
+{
+    internal static class ProtocolVersions
+    {
+        /// <summary>
+        /// Gets the current version of the discovery layer.
+        /// </summary>
+        public static Version Current
+        {
+            get
+            {
+                return V1;
+            }
+        }
+
+        /// <summary>
+        /// Gets the version of the base discovery layer.
+        /// </summary>
+        public static Version V1
+        {
+            get
+            {
+                return new Version(1, 0, 0, 0);
+            }
+        }
+
+        /// <summary>
+        /// Returns a collection containing all the supported versions of the discovery protocol.
+        /// </summary>
+        /// <returns>A collection containing all the supported versions of the discovery protocol.</returns>
+        public static IEnumerable<Version> SupportedVersions()
+        {
+            return new[]
+                {
+                    V1,
+                };
+        }
+    }
+}
