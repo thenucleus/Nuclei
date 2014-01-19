5785b767a8c175e8be3413bafe86a26c77a0cbdb
Petrik van der Velde <petrikvandervelde@gmail.com>
2014-01-20 08:55:26 +1300

    Adding the ID of the endpoint to the stored channel information.
    reference #3

--- src/nuclei.communication/ChannelInformation.cs
+++ src/nuclei.communication/ChannelInformation.cs
@@ -15,6 +15,11 @@ namespace Nuclei.Communication
     internal sealed class ChannelInformation
     {
         /// <summary>
+        /// The ID of the endpoint.
+        /// </summary>
+        private readonly EndpointId m_Id;
+
+        /// <summary>
         /// The version of the protocol for the current channel.
         /// </summary>
         private readonly Version m_ProtocolVersion;
@@ -27,23 +32,40 @@ namespace Nuclei.Communication
         /// <summary>
         /// Initializes a new instance of the <see cref="ChannelInformation"/> class.
         /// </summary>
+        /// <param name="id">The ID of the endpoint.</param>
         /// <param name="protocolVersion">The version of the protocol for the given channel.</param>
         /// <param name="address">The address of the given channel.</param>
+        /// <exception cref="ArgumentNullException">
+        ///     Thrown if <paramref name="id"/> is <see langword="null" />.
+        /// </exception>
         /// <exception cref="ArgumentNullException">
         ///     Thrown if <paramref name="protocolVersion"/> is <see langword="null" />.
         /// </exception>
         /// <exception cref="ArgumentNullException">
         ///     Thrown if <paramref name="address"/> is <see langword="null" />.
         /// </exception>
-        public ChannelInformation(Version protocolVersion, Uri address)
+        public ChannelInformation(EndpointId id, Version protocolVersion, Uri address)
         {
             {
+                Lokad.Enforce.Argument(() => id);
                 Lokad.Enforce.Argument(() => protocolVersion);
                 Lokad.Enforce.Argument(() => address);
             }
 
+            m_Id = id;
             m_ProtocolVersion = protocolVersion;
             m_Address = address;
+        }
+
+        /// <summary>
+        /// Gets the ID of the endpoint.
+        /// </summary>
+        public EndpointId Id
+        {
+            get
+            {
+                return m_Id;
+            }
         }
 
         /// <summary>
