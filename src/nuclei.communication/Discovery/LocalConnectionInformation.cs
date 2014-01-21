704a6bbd5fe6fa262313d952d07a2a198fc10146
Petrik van der Velde <petrikvandervelde@gmail.com>
2014-01-22 09:02:05 +1300

    Adding an object to store the connection information for the local endpoint. This object stores the discovery entry URI and the endpoint ID.
    references #3

---
+++ src/nuclei.communication/Discovery/LocalConnectionInformation.cs
@@ -0,0 +1,64 @@
+﻿//-----------------------------------------------------------------------
+// <copyright company="Nuclei">
+//     Copyright 2013 Nuclei. Licensed under the Apache License, Version 2.0.
+// </copyright>
+//-----------------------------------------------------------------------
+
+using System;
+
+namespace Nuclei.Communication.Discovery
+{
+    /// <summary>
+    /// Stores information about the connection information for the local endpoint.
+    /// </summary>
+    internal sealed class LocalConnectionInformation : IProvideLocalConnectionInformation
+    {
+        /// <summary>
+        /// The ID of the local endpoint.
+        /// </summary>
+        private readonly EndpointId m_Id;
+
+        /// <summary>
+        /// The URI of the local entry channel.
+        /// </summary>
+        private readonly Uri m_EntryChannel;
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="LocalConnectionInformation"/> class.
+        /// </summary>
+        /// <param name="id">The ID of the local endpoint.</param>
+        /// <param name="entryChannel">The URI of the local entry channel.</param>
+        public LocalConnectionInformation(EndpointId id, Uri entryChannel)
+        {
+            {
+                Lokad.Enforce.Argument(() => id);
+                Lokad.Enforce.Argument(() => entryChannel);
+            }
+
+            m_Id = id;
+            m_EntryChannel = entryChannel;
+        }
+
+        /// <summary>
+        /// Gets the ID of the local endpoint.
+        /// </summary>
+        public EndpointId Id
+        {
+            get
+            {
+                return m_Id;
+            }
+        }
+
+        /// <summary>
+        /// Gets the URI of the local entry channel.
+        /// </summary>
+        public Uri EntryChannel
+        {
+            get
+            {
+                return m_EntryChannel;
+            }
+        }
+    }
+}
---
+++ src/nuclei.communication/IProvideLocalConnectionInformation.cs
@@ -0,0 +1,32 @@
+﻿//-----------------------------------------------------------------------
+// <copyright company="Nuclei">
+//     Copyright 2013 Nuclei. Licensed under the Apache License, Version 2.0.
+// </copyright>
+//-----------------------------------------------------------------------
+
+using System;
+
+namespace Nuclei.Communication
+{
+    /// <summary>
+    /// Defines the interface for objects that provide the connection information for the local endpoint.
+    /// </summary>
+    public interface IProvideLocalConnectionInformation
+    {
+        /// <summary>
+        /// Gets the ID of the local endpoint.
+        /// </summary>
+        EndpointId Id
+        {
+            get;
+        }
+
+        /// <summary>
+        /// Gets the URI of the local entry channel.
+        /// </summary>
+        Uri EntryChannel
+        {
+            get;
+        }
+    }
+}
