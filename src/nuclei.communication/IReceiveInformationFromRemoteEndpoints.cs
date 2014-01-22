e0d3f65ef6311a5493a6c86437817e476a9fe92c
Petrik van der Velde <petrikvandervelde@gmail.com>
2014-01-23 07:52:52 +1300

    Adding design related comment to the IReceiveInformationFromRemoteEndpoints interface

--- src/nuclei.communication/IReceiveInformationFromRemoteEndpoints.cs
+++ src/nuclei.communication/IReceiveInformationFromRemoteEndpoints.cs
@@ -11,6 +11,10 @@ namespace Nuclei.Communication
     /// <summary>
     /// Defines the base interface for objects that act as message or data receiving proxies for a <see cref="ServiceHost"/>.
     /// </summary>
+    /// <design>
+    /// This interface forms the base interface for all interfaces / classes that define a receiving endpoint,
+    /// for both the discovery and the protocol layers.
+    /// </design>
     internal interface IReceiveInformationFromRemoteEndpoints
     {
     }
