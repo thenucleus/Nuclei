4c68d1d7c2c974c94e6452340698265f239818b2
Petrik van der Velde <petrikvandervelde@gmail.com>
2014-01-23 08:29:13 +1300

    Making the communication layer interface internal because it is no longer needed to be public now that the ICommunicationFacade object exists.
    references #3

--- src/nuclei.communication/Protocol/ICommunicationLayer.cs
+++ src/nuclei.communication/Protocol/ICommunicationLayer.cs
@@ -7,13 +7,15 @@
 using System;
 using System.Collections.Generic;
 using System.Diagnostics.CodeAnalysis;
+using System.Threading;
+using System.Threading.Tasks;
 
 namespace Nuclei.Communication.Protocol
 {
     /// <summary>
     /// Defines the methods for communicating with a remote endpoint.
     /// </summary>
-    public interface ICommunicationLayer
+    internal interface ICommunicationLayer
     {
         /// <summary>
         /// Gets the endpoint ID of the local endpoint.
@@ -92,5 +94,45 @@ namespace Nuclei.Communication.Protocol
         [SuppressMessage("Microsoft.StyleCop.CSharp.DocumentationRules", "SA1628:DocumentationTextMustBeginWithACapitalLetter",
             Justification = "Documentation can start with a language keyword")]
         bool IsEndpointContactable(EndpointId endpoint);
+
+        /// <summary>
+        /// Returns a collection containing information about the local connection points.
+        /// </summary>
+        /// <returns>
+        /// The collection that describes the local connection points.
+        /// </returns>
+        IEnumerable<ChannelConnectionInformation> LocalConnectionPoints();
+
+        /// <summary>
+        /// Sends the given message to the specified endpoint.
+        /// </summary>
+        /// <param name="endpoint">The endpoint to which the message has to be send.</param>
+        /// <param name="message">The message that has to be send.</param>
+        void SendMessageTo(EndpointId endpoint, ICommunicationMessage message);
+
+        /// <summary>
+        /// Sends the given message to the specified endpoint and returns a task that
+        /// will eventually contain the return message.
+        /// </summary>
+        /// <param name="endpoint">The endpoint to which the message has to be send.</param>
+        /// <param name="message">The message that has to be send.</param>
+        /// <returns>A task object that will eventually contain the response message.</returns>
+        Task<ICommunicationMessage> SendMessageAndWaitForResponse(EndpointId endpoint, ICommunicationMessage message);
+
+        /// <summary>
+        /// Uploads a given file to a specific endpoint.
+        /// </summary>
+        /// <param name="receivingEndpoint">The endpoint that will receive the data stream.</param>
+        /// <param name="filePath">The full path to the file that should be transferred.</param>
+        /// <param name="token">The cancellation token that is used to cancel the task if necessary.</param>
+        /// <param name="scheduler">The scheduler that is used to run the return task.</param>
+        /// <returns>
+        ///     A task that will return once the upload is complete.
+        /// </returns>
+        Task UploadData(
+            EndpointId receivingEndpoint,
+            string filePath,
+            CancellationToken token,
+            TaskScheduler scheduler);
     }
 }
