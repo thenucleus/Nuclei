746a28a1ffa910c275e7c42086c89f71cbd26026
Petrik van der Velde <petrikvandervelde@gmail.com>
2014-01-29 20:09:44 +1300

    Adding extension methods for the Uri class.
    references #3

---
+++ src/nuclei.communication/Protocol/UriExtensions.cs
@@ -0,0 +1,38 @@
+﻿//-----------------------------------------------------------------------
+// <copyright company="Nuclei">
+//     Copyright 2013 Nuclei. Licensed under the Apache License, Version 2.0.
+// </copyright>
+//-----------------------------------------------------------------------
+
+using System;
+
+namespace Nuclei.Communication.Protocol
+{
+    /// <summary>
+    /// Defines a set of extension methods for the <see cref="Uri"/> class.
+    /// </summary>
+    internal static class UriExtensions
+    {
+        /// <summary>
+        /// Gets the <see cref="ChannelTemplate"/> from a <see cref="Uri"/>.
+        /// </summary>
+        /// <param name="uri">The URI from which the template should be obtained.</param>
+        /// <returns>The channel template that matches the given URI.</returns>
+        public static ChannelTemplate ToChannelTemplate(this Uri uri)
+        {
+            switch (uri.Scheme)
+            {
+                case "net.pipe":
+                    return ChannelTemplate.NamedPipe;
+                case "net.tcp":
+                    return ChannelTemplate.TcpIP;
+                case "http":
+                    return ChannelTemplate.Http;
+                case "https":
+                    return ChannelTemplate.Https;
+                default:
+                    return ChannelTemplate.Unknown;
+            }
+        }
+    }
+}
