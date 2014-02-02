8d1004c2a237a3619b411f022e84ca908ae01e17
Petrik van der Velde <petrikvandervelde@gmail.com>
2014-02-02 15:14:43 +1300

    Moving IStoreInformationABoutEndpoints to the main namespace
    references #3

--- src/nuclei.communication/IStoreInformationAboutEndpoints.cs
+++ src/nuclei.communication/IStoreInformationAboutEndpoints.cs
@@ -7,7 +7,7 @@
 using System.Collections.Generic;
 using System.Diagnostics.CodeAnalysis;
 
-namespace Nuclei.Communication.Protocol
+namespace Nuclei.Communication
 {
     /// <summary>
     /// Defines the interface for objects that store information about all known endpoints.
