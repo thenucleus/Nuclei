0ed5aec49d0a3b1dc601bb01d6d08875657de770
Petrik van der Velde <petrikvandervelde@gmail.com>
2014-05-07 22:46:20 +1200

    Adding the delegates for sending of messages.
    references #23

---
+++ src/nuclei.communication/Protocol/SendMessage.cs
@@ -0,0 +1,18 @@
+﻿//-----------------------------------------------------------------------
+// <copyright company="Nuclei">
+//     Copyright 2013 Nuclei. Licensed under the Apache License, Version 2.0.
+// </copyright>
+//-----------------------------------------------------------------------
+
+namespace Nuclei.Communication.Protocol
+{
+    /// <summary>
+    /// Defines the signature for functions that send messages without waiting for a response.
+    /// </summary>
+    /// <param name="endpoint">The endpoint to which the message is send.</param>
+    /// <param name="message">The message.</param>
+    /// <param name="maximumNumberOfRetries">
+    /// The maximum number of times the endpoint will try to send the message if delivery fails. 
+    /// </param>
+    internal delegate void SendMessage(EndpointId endpoint, ICommunicationMessage message, int maximumNumberOfRetries);
+}
---
+++ src/nuclei.communication/Protocol/SendMessageAndWaitForResponse.cs
@@ -0,0 +1,22 @@
+﻿//-----------------------------------------------------------------------
+// <copyright company="Nuclei">
+//     Copyright 2013 Nuclei. Licensed under the Apache License, Version 2.0.
+// </copyright>
+//-----------------------------------------------------------------------
+
+using System;
+using System.Threading.Tasks;
+
+namespace Nuclei.Communication.Protocol
+{
+    /// <summary>
+    /// Defines the signature for functions that send messages and then wait for a response.
+    /// </summary>
+    /// <param name="endpoint">The endpoint to which the message is send.</param>
+    /// <param name="message">The message.</param>
+    /// <param name="timeout">The maximum amount of time the response operation is allowed to take.</param>
+    /// <returns>
+    /// A task object that will eventually contain the response message.
+    /// </returns>
+    internal delegate Task<ICommunicationMessage> SendMessageAndWaitForResponse(EndpointId endpoint, ICommunicationMessage message, TimeSpan timeout);
+}
