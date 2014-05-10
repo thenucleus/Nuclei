894ecb359b6e2369dcd9161f7cfe7492a86d1e1e
Petrik van der Velde <petrikvandervelde@gmail.com>
2014-05-10 12:32:12 +1200

    Adding the retry count to the send-with-response delegate.
    references #23

--- src/nuclei.communication/Protocol/SendMessageAndWaitForResponse.cs
+++ src/nuclei.communication/Protocol/SendMessageAndWaitForResponse.cs
@@ -14,9 +14,16 @@ namespace Nuclei.Communication.Protocol
     /// </summary>
     /// <param name="endpoint">The endpoint to which the message is send.</param>
     /// <param name="message">The message.</param>
+    /// <param name="maximumNumberOfRetries">
+        /// The maximum number of times the endpoint will try to send the message if delivery fails. 
+        /// </param>
     /// <param name="timeout">The maximum amount of time the response operation is allowed to take.</param>
     /// <returns>
     /// A task object that will eventually contain the response message.
     /// </returns>
-    internal delegate Task<ICommunicationMessage> SendMessageAndWaitForResponse(EndpointId endpoint, ICommunicationMessage message, TimeSpan timeout);
+    internal delegate Task<ICommunicationMessage> SendMessageAndWaitForResponse(
+        EndpointId endpoint, 
+        ICommunicationMessage message, 
+        int maximumNumberOfRetries,
+        TimeSpan timeout);
 }
