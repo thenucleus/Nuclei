4e9ce77ea87a9e09acd9920dec0935a1c698a4df
Petrik van der Velde <petrikvandervelde@gmail.com>
2014-05-10 21:29:14 +1200

    Adding the attributes used to indicate retry count and time-out values on the command interface.
    reference #23

---
+++ src/nuclei.communication/Interaction/InvocationTimeoutAttribute.cs
@@ -0,0 +1,29 @@
+﻿//-----------------------------------------------------------------------
+// <copyright company="Nuclei">
+//     Copyright 2013 Nuclei. Licensed under the Apache License, Version 2.0.
+// </copyright>
+//-----------------------------------------------------------------------
+
+using System;
+
+namespace Nuclei.Communication.Interaction
+{
+    /// <summary>
+    /// Defines an attribute that indicates that an parameter on an interface method is supposed to
+    /// be the maximum time in milliseconds a command invocation may take before timing out.
+    /// </summary>
+    [AttributeUsage(AttributeTargets.Parameter, AllowMultiple = false, Inherited = true)]
+    public sealed class InvocationTimeoutAttribute : CommandProxyParameterUsageAttribute
+    {
+        /// <summary>
+        /// Gets the type that the parameter on which the current attribute is applied should have.
+        /// </summary>
+        public override Type AllowedParameterType
+        {
+            get
+            {
+                return typeof(int);
+            }
+        }
+    }
+}
