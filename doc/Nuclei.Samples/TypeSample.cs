11c19efed1cd8255b8d8f3a44de730bcdcf48435
Petrik van der Velde <petrikvandervelde@gmail.com>
2016-07-18 19:58:10 +1200

    Adding the samples project
    references #12

---
+++ doc/Nuclei.Samples/TypeSample.cs
@@ -0,0 +1,44 @@
+﻿//-----------------------------------------------------------------------
+// <copyright company="TheNucleus">
+// Copyright (c) TheNucleus. All rights reserved.
+// Licensed under the Apache License, Version 2.0 license. See LICENCE.md file in the project root for full license information.
+// </copyright>
+//-----------------------------------------------------------------------
+
+using System;
+using System.Collections.Generic;
+using System.Diagnostics.CodeAnalysis;
+using NUnit.Framework;
+
+namespace Nuclei.Samples
+{
+    [TestFixture]
+    [SuppressMessage(
+        "Microsoft.StyleCop.CSharp.DocumentationRules",
+        "SA1600:ElementsMustBeDocumented",
+        Justification = "Unit tests do not need documentation.")]
+    public sealed class TypeSample
+    {
+        [Test]
+        public void IsAssignableToOpenGenericType()
+        {
+            {
+                // Should return true
+                var result = typeof(List<>).IsAssignableToOpenGenericType(typeof(List<int>));
+                Assert.IsTrue(result);
+            }
+
+            {
+                // Should return false
+                var result = typeof(IList<>).IsAssignableToOpenGenericType(typeof(Dictionary<int, int>));
+                Assert.IsFalse(result);
+            }
+
+            {
+                // Should return true
+                var result = typeof(IComparable<>).IsAssignableToOpenGenericType(typeof(bool));
+                Assert.IsTrue(result);
+            }
+        }
+    }
+}
